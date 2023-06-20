cdd93ab526d38de87e302888da10a183d7ac4f93
Felix-9999 <105433860+Felix-9999@users.noreply.github.com>
2023-06-20 16:13:39 +0400

    nor opopxutyun

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,19 +1,15 @@
-// api/users/index.js
-
 import connectMongo from "../../../database/connTable"
 import { getUsers, postUser, putUser, deleteUser } from "../../../database/controllerTable"
-import Cors from 'cors';
-
-const cors = Cors({
-  origin: 'https://crud-rosy-two.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-});
 
 export default async function handler(req, res) {
-  await cors(req, res); // Use CORS middleware
-
   connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection" }));
 
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'https://crud-rosy-two.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Type of request
   const { method } = req;
 
   switch (method) {
