e6ad015c13542dd0d68aff3e5ea261d67179eea3
Felix-9999 <105433860+Felix-9999@users.noreply.github.com>
2023-06-21 14:12:46 +0400

    adds

--- pages/api/users.js
+++ pages/api/users.js
@@ -3,6 +3,7 @@ export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Private-Network', 'true'); // Add this line
   
     if (req.method === 'OPTIONS') {
       // Handle preflight request
