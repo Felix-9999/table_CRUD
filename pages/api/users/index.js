93921e1dbf962a4a092814c7a035ba429e6be4a0
Felix-9999 <105433860+Felix-9999@users.noreply.github.com>
2023-06-21 13:13:55 +0400

    cors

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,28 +1,24 @@
-import connectMongo from "../../../database/connTable"
-import { getUsers, postUser, putUser, deleteUser } from "../../../database/controllerTable"
+import connectMongo from '../../../database/connTable';
+import { getUsers, postUser, putUser, deleteUser } from '../../../database/controllerTable';
 
 export default async function handler(req, res) {
-  connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection"}))
+  connectMongo().catch(() => res.status(405).json({ error: 'Error in the Connection' }));
 
   // type of request
-  const { method } = req
+  const { method } = req;
 
-  switch(method){
-      case 'GET' :
-          getUsers(req, res)
-          break;
-      case 'POST':
-          postUser(req, res)
-          break;
-      case 'PUT':
-          putUser(req, res)
-          break;
-      case 'DELETE':
-          deleteUser(req, res)
-          break;
-      default : 
-          res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
-          res.status(405).end(`Method ${method} Not Allowd`)
-          break;
+  switch (method) {
+    case 'GET':
+      return corsMiddleware(req, res, () => getUsers(req, res));
+    case 'POST':
+      return corsMiddleware(req, res, () => postUser(req, res));
+    case 'PUT':
+      return corsMiddleware(req, res, () => putUser(req, res));
+    case 'DELETE':
+      return corsMiddleware(req, res, () => deleteUser(req, res));
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
   }
 }
